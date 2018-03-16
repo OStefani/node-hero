6e290148028b2108acb7b6db660fa1dff032a1b6
OStefani <ostefanishina@gmail.com>
2018-03-16 21:19:29 +0300

    nextTick

--- 04-nodejs-modules/next_tick/index_2.js
+++ 04-nodejs-modules/next_tick/index_2.js
@@ -1,15 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     res.end('Hi');
-    //setTimout не гарантирует, что асинхронный код будет выполнен до того, как прийдет следующий //запрос
-    //setTimeout(() => {}, 0);
+    fs.open('../view/index.html', 'r', (err, file) => {
+        if (err) throw err;
+        console.log(`Opened!`);
+    });
     process.nextTick(() => {
-        req.on('readable', function() {
+        //req.on('readable', function() {
             console.log(`Second - nextTick`);
             //должен сработать сразу после выполнения js (на ближайщих данных)
-        });
+        //});
     });
+    //setTimout не гарантирует, что асинхронный код будет выполнен до того, как прийдет следующий //запрос
+    //setTimeout(() => {}, 0);
+    setTimeout(() => {
+        console.log(`Forth - settimout?`);
+    }, 0);
+
+    //позволяе вызывать код ассинхронно и не тормозить событийный цикл
+    //выставляет в очередь сразу после колбеков событий ввода-вывода
+    /**
+     * callbacks for some system operations such as types 
+     * of TCP errors. For example if a TCP socket receives ECONNREFUSED when attempting
+     * to connect, some *nix systems want to wait to report the error. This will 
+     * be queued to execute in the I/O callbacks phase.
+     */
     setImmediate(() => {
         console.log(`Third - immediate`);
     });
