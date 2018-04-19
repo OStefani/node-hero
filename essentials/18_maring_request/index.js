f9ddedaf1e6d3dd0f863c1d1112b3afb03735744
OStefani <ostefanishina@gmail.com>
2018-04-19 16:33:40 +0300

    Making request

---
+++ essentials/18_maring_request/index.js
@@ -0,0 +1,42 @@
+// to make a request to a sequire server
+const https = require('https');
+const fs = require('fs');
+
+options = {
+    hostname: 'en.wikipedia.org',
+    port: 443,
+    path: '/wiki/George_Washington',
+    method: 'GET'
+};
+
+var req = https.request(options, (res) => {
+    var resBody = '';
+    console.log('Response from server started');
+    console.log(`Server status: ${res.statusCode}`);
+    console.log("Response Headers: %j", res.headers);
+    res.setEncoding("UTF-8");
+
+	res.once("data", function(chunk) {
+		console.log(chunk);
+	});
+
+	res.on("data", function(chunk) {
+		console.log(`--chunk-- ${chunk.length}`);
+		resBody += chunk;
+	});
+
+	res.on("end", function() {
+		fs.writeFile("george-washington.html", resBody, function(err) {
+			if (err) {
+				throw err;
+			}
+			console.log("File Downloaded");
+		});
+	});
+});
+
+req.on("error", function(err) {
+	console.log(`problem with request: ${err.message}`);
+});
+
+req.end();
